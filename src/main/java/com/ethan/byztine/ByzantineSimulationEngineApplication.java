770d416ca50cbca3c6bbbe6c04cf44aab3ed271f
Ethan Carranza Serrano <ethan.carranza@fero.com>
2026-02-25 22:36:43 +0100

    Initial Spring Boot project setup

---
+++ src/main/java/com/ethan/byztine/ByzantineSimulationEngineApplication.java
@@ -0,0 +1,13 @@
+package com.ethan.byztine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ByzantineSimulationEngineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ByzantineSimulationEngineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ethan/byztine/ByzantineSimulationEngineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ethan.byztine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ByzantineSimulationEngineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
